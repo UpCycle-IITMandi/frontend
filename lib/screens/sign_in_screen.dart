fe54295371d12c959ca348d60c0609f4c1258376
Gajraj Singh Chouhan <gajrajgchouhan@gmail.com>
2022-08-22 22:23:39 +0530

    Added sign in page and firebase check.

---
+++ lib/screens/sign_in_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+// import 'package:frontend/utils/authentication.dart';
+
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade400,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 20.0,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 20),
+                    Text(
+                      'FlutterFire',
+                      style: TextStyle(
+                        color: Colors.yellow.shade400,
+                        fontSize: 40,
+                      ),
+                    ),
+                    Text(
+                      'Authentication',
+                      style: TextStyle(
+                        color: Colors.orange.shade400,
+                        fontSize: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // FutureBuilder(
+              //   future: Authentication.initializeFirebase(context: context),
+              //   builder: (context, snapshot) {
+              //     if (snapshot.hasError) {
+              //       return Text('Error initializing Firebase');
+              //     } else if (snapshot.connectionState == ConnectionState.done) {
+              //       return GoogleSignInButton();
+              //     }
+              //     return CircularProgressIndicator(
+              //       valueColor: AlwaysStoppedAnimation<Color>(
+              //         CustomColors.firebaseOrange,
+              //       ),
+              //     );
+              //   },
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

882070acc412fb03f3741f1be2326a4f0a8cb31f
Gajraj Singh Chouhan <gajrajgchouhan@gmail.com>
2022-08-24 16:19:33 +0530

    Added google sign in button

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,6 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-// import 'package:frontend/utils/authentication.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 class SignInScreen extends StatefulWidget {
   @override
@@ -20,47 +20,25 @@ class _SignInScreenState extends State<SignInScreen> {
             bottom: 20.0,
           ),
           child: Column(
-            mainAxisSize: MainAxisSize.max,
             children: [
-              Row(),
-              Expanded(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(height: 20),
-                    Text(
-                      'FlutterFire',
-                      style: TextStyle(
-                        color: Colors.yellow.shade400,
-                        fontSize: 40,
-                      ),
-                    ),
-                    Text(
-                      'Authentication',
-                      style: TextStyle(
-                        color: Colors.orange.shade400,
-                        fontSize: 40,
-                      ),
-                    ),
-                  ],
+              Text(
+                'UpCycle',
+                style: TextStyle(
+                  color: Colors.orange.shade400,
+                  fontSize: 20,
                 ),
               ),
-              // FutureBuilder(
-              //   future: Authentication.initializeFirebase(context: context),
-              //   builder: (context, snapshot) {
-              //     if (snapshot.hasError) {
-              //       return Text('Error initializing Firebase');
-              //     } else if (snapshot.connectionState == ConnectionState.done) {
-              //       return GoogleSignInButton();
-              //     }
-              //     return CircularProgressIndicator(
-              //       valueColor: AlwaysStoppedAnimation<Color>(
-              //         CustomColors.firebaseOrange,
-              //       ),
-              //     );
-              //   },
-              // ),
+              SignInButton(Buttons.Google, onPressed: () {
+                var googleProvider = GoogleAuthProvider();
+                googleProvider.addScope(
+                    'https://www.googleapis.com/auth/contacts.readonly');
+                googleProvider
+                    .setCustomParameters({'login_hint': 'user@example.com'});
+
+                FirebaseAuth.instance
+                    .signInWithAuthProvider(googleProvider)
+                    .then((value) => print(value));
+              }),
             ],
           ),
         ),

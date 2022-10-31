a0da98a2811300e8fbab2ee097f0ce3dc9707b04
Gajraj Singh Chouhan <54475046+gajrajgchouhan@users.noreply.github.com>
2022-10-31 15:55:24 +0530

    Added cart page. (#3)
    * Added cart features.
    
    * Fixed image size in vendor screen and vendor id.
    
    * Align name of Vendor.

--- lib/screens/Cart/cart_screen.dart
+++ lib/screens/Cart/cart_screen.dart
@@ -36,107 +36,164 @@ class _CartScreenState extends State<CartScreen> {
           StoreConnector<AppState, List<CartItem>>(
               converter: (store) => store.state.cartItems,
               builder: (context, cartItems) {
+                if (cartItems.isEmpty) {
+                  // empty cart message
+                  return Align(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Empty Cart.",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  );
+                }
                 final currency = NumberFormat.simpleCurrency(
                     locale: 'en_IN', name: 'INR', decimalDigits: 2);
-                return Expanded(
-                    child: ListView.builder(
-                        itemCount: cartItems.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                              margin: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 5),
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 5),
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: Row(
-                                children: [
-                                  Container(
-                                    height: 100,
-                                    width: 100,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10),
-                                      image: DecorationImage(
-                                        image: NetworkImage(
-                                            cartItems[index].product.images[0]),
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
+                return SizedBox(
+                  height: MediaQuery.of(context).size.height - 100,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: ListView.builder(
+                            itemCount: cartItems.length,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 5),
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 5),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(10),
                                   ),
-                                  const SizedBox(width: 10),
-                                  Expanded(
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          cartItems[index].product.productName,
-                                          style: const TextStyle(
-                                            fontSize: 12,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                        const SizedBox(height: 5),
-                                        Text(
-                                          currency.format(
-                                              cartItems[index].product.price),
-                                          style: TextStyle(
-                                            fontSize: 9,
-                                            color: Colors.orangeAccent.shade200,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                  Column(
+                                  child: Row(
                                     children: [
-                                      IconButton(
-                                        onPressed: () {
-                                          StoreProvider.of<AppState>(context)
-                                              .dispatch(MyAction(
-                                                  CartItem(
-                                                      cartItems[index].product,
-                                                      cartItems[index]
-                                                              .quantity +
-                                                          1),
-                                                  CartActions.EditCartAction));
-                                        },
-                                        icon: const Icon(
-                                          Icons.add,
-                                          color: Colors.black,
+                                      Container(
+                                        height: 100,
+                                        width: 100,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(10),
+                                          image: DecorationImage(
+                                            image: NetworkImage(cartItems[index]
+                                                .product
+                                                .images[0]),
+                                            fit: BoxFit.cover,
+                                          ),
                                         ),
                                       ),
-                                      Text(
-                                        cartItems[index].quantity.toString(),
-                                        style: const TextStyle(
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.bold,
+                                      const SizedBox(width: 10),
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              cartItems[index]
+                                                  .product
+                                                  .productName,
+                                              style: const TextStyle(
+                                                fontSize: 12,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                            const SizedBox(height: 5),
+                                            Text(
+                                              currency.format(cartItems[index]
+                                                  .product
+                                                  .price),
+                                              style: TextStyle(
+                                                fontSize: 9,
+                                                color: Colors
+                                                    .orangeAccent.shade200,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ],
                                         ),
                                       ),
-                                      IconButton(
-                                        onPressed: () {
-                                          StoreProvider.of<AppState>(context)
-                                              .dispatch(MyAction(
-                                                  CartItem(
-                                                      cartItems[index].product,
-                                                      cartItems[index]
-                                                              .quantity -
-                                                          1),
-                                                  CartActions.EditCartAction));
-                                        },
-                                        icon: const Icon(
-                                          Icons.remove,
-                                          color: Colors.black,
-                                        ),
-                                      ),
+                                      Column(
+                                        children: [
+                                          IconButton(
+                                            onPressed: () {
+                                              StoreProvider.of<AppState>(
+                                                      context)
+                                                  .dispatch(MyAction(
+                                                      CartItem(
+                                                          cartItems[index]
+                                                              .product,
+                                                          cartItems[index]
+                                                                  .quantity +
+                                                              1),
+                                                      CartActions
+                                                          .EditCartAction));
+                                            },
+                                            icon: const Icon(
+                                              Icons.add,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                          Text(
+                                            cartItems[index]
+                                                .quantity
+                                                .toString(),
+                                            style: const TextStyle(
+                                              fontSize: 12,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          IconButton(
+                                            onPressed: () {
+                                              StoreProvider.of<AppState>(
+                                                      context)
+                                                  .dispatch(MyAction(
+                                                      CartItem(
+                                                          cartItems[index]
+                                                              .product,
+                                                          cartItems[index]
+                                                                  .quantity -
+                                                              1),
+                                                      CartActions
+                                                          .EditCartAction));
+                                            },
+                                            icon: const Icon(
+                                              Icons.remove,
+                                              color: Colors.black,
+                                            ),
+                                          ),
+                                        ],
+                                      )
                                     ],
-                                  )
-                                ],
-                              ));
-                        }));
+                                  ));
+                            }),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: TextButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                                Colors.orangeAccent.shade400),
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    side: BorderSide(
+                                        color: Colors.orangeAccent.shade400))),
+                          ),
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                                "${currency.format(cartItems.fold<int>(0, (previousValue, element) => previousValue + element.product.price * element.quantity))} | Proceed to Checkout",
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                );
               })
         ]));
   }
